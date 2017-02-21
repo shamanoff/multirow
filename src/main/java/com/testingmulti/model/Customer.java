e8b8032eba793e174860638c13d7c6bc0c2fc180
Michael <shamanoff@gmail.com>
2017-02-21 08:55:18 +0200

    home

--- src/main/java/com/testingmulti/model/Customer.java
+++ src/main/java/com/testingmulti/model/Customer.java
@@ -4,12 +4,51 @@ import java.io.Serializable;
 
 public class Customer implements Serializable {
 
-	
-	private static final long serialVersionUID = 1L;
-	private String name;
 
-	public Customer(){
-	}
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private Long age;
+
+    public Customer(String name, Long age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public Customer setAge(Long age) {
+        this.age = age;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    /*	public Customer(){
+    }
 
 	public Customer(String name){
 		this.name = name;
@@ -26,5 +65,5 @@ public class Customer implements Serializable {
 	@Override
 	public String toString() {
 		return "{" + name  + "}";
-	}
+	}*/
 }
